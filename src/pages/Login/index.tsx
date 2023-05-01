b6f257774a35d6e6f410866b910897f322b439bc
joseernanejob <jose.ernane.job@gmail.com>
2023-05-01 19:13:38 -0300

    feat: add style mobile login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,18 @@
 import LoginForm from '../../components/Form/LoginForm/LoginForm';
+import Logo from '../../assets/imgs/logoSpaceSound.svg';
+import { StyleLoginPage } from './style';
 
 const Login = () => {
   return (
-    <>
+    <StyleLoginPage>
+      <img src={Logo} alt='Logo SoundSpace' />
+      <p>
+        “Conecte-se com a sua música, compartilhe com o mundo e seja a estrela
+        do seu próprio show”
+      </p>
+      <h2>Login</h2>
       <LoginForm />
-    </>
+    </StyleLoginPage>
   );
 };
 
