b2567eaf68789e4a283e72414669fcc573cdd87e
joseernanejob <jose.ernane.job@gmail.com>
2023-05-01 10:23:07 -0300

    Initial commit

---
+++ src/providers/User/UserContext.tsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IUserContextProps } from './@types';
+
+export const UserContext = createContext({});
