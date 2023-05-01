02eb3899b4d18f321b97261007fec61cf1a98c40
joseernanejob <jose.ernane.job@gmail.com>
2023-05-01 17:49:14 -0300

    feat: login form

---
+++ src/components/Form/LoginForm/LoginForm.tsx
@@ -0,0 +1,13 @@
+import { LoginFormContainer } from './style';
+
+const LoginForm = () => {
+  const test = 0;
+
+  return (
+    <LoginFormContainer>
+      <h2>test</h2>
+    </LoginFormContainer>
+  );
+};
+
+export default LoginForm;
