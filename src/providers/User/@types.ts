c0e89f148978e7d454387b421ffe50fdc4f9b3a6
joseernanejob <jose.ernane.job@gmail.com>
2023-05-01 11:06:06 -0300

    feat: request login user

--- src/providers/User/@types.ts
+++ src/providers/User/@types.ts
@@ -1,3 +1,10 @@
 export interface IUserContextProps {
   children: React.ReactNode;
 }
+
+export interface IUser {
+  email: string;
+  name: string;
+  avatar: string;
+  id: number;
+}
--- src/providers/User/UserContext.tsx
+++ src/providers/User/UserContext.tsx
@@ -1,14 +1,19 @@
 import { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { IUserContextProps } from './@types';
+import axios from 'axios';
+import { IUser, IUserContextProps } from './@types';
 
 export const UserContext = createContext({});
 
 export const UserProvider = ({ children }: IUserContextProps) => {
-  const [User, setUser] = useState();
+  const [User, setUser] = useState<IUser>();
+  const [token, setToken] = useState<string>('');
 
-  const UserLogin = async () => {
-    console.log('test');
+  const UserLogin = async (data: object) => {
+    const GetUser = axios
+      .post('https://soundspaceapi.onrender.com/login', data)
+      .then((response) => {
+        console.log(response);
+      });
   };
 
   return (
