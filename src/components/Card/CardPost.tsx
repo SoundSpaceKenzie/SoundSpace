fc5ce3b9ba4c8148b5889e2c95de3b861af28458
Igor Pontes de Oliveira <igorpontes8@gmail.com>
2023-05-03 14:54:32 -0300

    feat:CardPost

--- src/components/Card/CardPost.tsx
+++ src/components/Card/CardPost.tsx
@@ -3,6 +3,11 @@ import { UserContext } from "../../providers/User/UserContext";
 import { useSearchParams } from "react-router-dom";
 import { IUser } from "../../providers/User/@types";
 import { Spotify } from "react-spotify-embed";
+import ReactPlayer from "react-player";
+import { FiHeart } from 'react-icons/fi'
+import { BiCommentDetail } from 'react-icons/bi'
+
+
 interface IProps {
   url: string;
   description: string;
@@ -35,17 +40,23 @@ export const CardPost = ({ url, idPost, description }: IProps) => {
         
       </div>
 
-      <div>
+      <div className="CardMedia">
         {url}
-        <Spotify link={url}></Spotify>
+        <Spotify link="https://open.spotify.com/track/0gplL1WMoJ6iYaPgMCL0gX?si=7fc1c19e6c2e4208" width={300} height={255}></Spotify>
+        <ReactPlayer className='react-player'url='https://music.youtube.com/watch?v=-eGM0IJc70Y&feature=share' width='100%' height='250px' controls='true'
+      />
       </div>
         
-
+     <div className="Icons" >
+     <FiHeart /> <BiCommentDetail />
+     </div>
+     
       <p>{description}</p>
-      
+
+     
       
     </li>
-
+     
   )
 }
 
