a2e1d4696b996f06995734cb2b4df8e95fac5b46
joseernanejob <jose.ernane.job@gmail.com>
2023-05-03 14:52:53 -0300

    fix: change bugs cards posts

--- src/components/Card/CardPost.tsx
+++ src/components/Card/CardPost.tsx
@@ -1,51 +1,29 @@
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../../providers/User/UserContext";
-import { useSearchParams } from "react-router-dom";
-import { IUser } from "../../providers/User/@types";
-import { Spotify } from "react-spotify-embed";
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../providers/User/UserContext';
+import { useSearchParams } from 'react-router-dom';
+import { IUser } from '../../providers/User/@types';
+import { Spotify } from 'react-spotify-embed';
 interface IProps {
   url: string;
   description: string;
-  idPost: number;
-  
+  name: string;
+  avatar: string;
 }
 
-
-
-export const CardPost = ({ url, idPost, description }: IProps) => {
-  // const {Users} = useContext(UserContext)
-
-  // const [user, setUser ] = useState <IUser[]> ()
-  // useEffect(() =>{
-  //      setUser(Users.filter(element =>{
-  //       return element.id === idPost
-  //      }))
-  // }, [])
-
-
-
-  
-  
-  return(
+export const CardPost = ({ name, avatar, url, description }: IProps) => {
+  return (
     <li>
       <div>
-        {/* <img src={user.avatar} alt="Usuário" />
-        <p>{name}</p> */}
-        
-        
+        <img src={avatar} alt='Usuário' />
+        <p>{name}</p>
       </div>
 
       <div>
-        {url}
-        <Spotify link={url}></Spotify>
+        {/* {url}
+        <Spotify link={url}></Spotify> */}
       </div>
-        
 
       <p>{description}</p>
-      
-      
     </li>
-
-  )
-}
-
+  );
+};
--- src/providers/Post/@types.ts
+++ src/providers/Post/@types.ts
@@ -1,30 +1,31 @@
-export interface IPostProps{
-  children: React.ReactNode
+export interface IPostProps {
+  children: React.ReactNode;
 }
 
-export interface IPostContext{
-  posts: IPost[]
+export interface IPostContext {
+  posts: IPost[];
 }
 
-
-
-
-export interface IData{
-  data: [IPost]
-
+export interface IData {
+  data: [IPost];
 }
 
-export interface IPost{
+export interface IPost {
   description: string;
   id: number;
   like: [ILike];
   link: string;
   type: string;
   name: string;
-  useId: number;
+  user: IUser;
+  userId: number;
 }
 
 export interface ILike {
   useID: number;
+}
 
+interface IUser {
+  name: string;
+  avatar: string;
 }
